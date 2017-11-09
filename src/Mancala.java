b0ce7fdfea84c01f5a5871b0a41eec20555be323
Marissa Xiong <marxiong5@gmail.com>
2017-11-09 12:32:06 -0800

    Add files via upload

--- src/Mancala.java
+++ src/Mancala.java
@@ -1,40 +1,27 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class Mancala implements Icon{
-
-
-	public Mancala() {
-
-	}	
-
-	@Override
-	public int getIconHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getIconWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+public class Mancala{
+	ArrayList<Ellipse2D.Double> pockets;
+	JPanel label = new JPanel();
+	int startX = 150;
+	int startY = 100;
+	Rectangle2D.Double outline = new Rectangle2D.Double(startX, startY, 600, 180);
+	Rectangle2D.Double pocket1 = new Rectangle2D.Double(startX + 20, startY + 20, 60, 140);
+	Rectangle2D.Double pocket2 = new Rectangle2D.Double(startX + 520, startY + 20, 60, 140);
 	
 	
-	@Override
-	public void paintIcon(Component arg0, Graphics g, int x, int y) {
+	public Mancala(){
+		pockets = new ArrayList<>();
+	}
+
+	public void draw(Graphics g){
 		Graphics2D g2 = (Graphics2D) g;
-		int startX = 250;
-		int startY = 100;
-		Rectangle2D.Double outline = new Rectangle2D.Double(startX, startY, 600, 180);
-		Rectangle2D.Double pocket1 = new Rectangle2D.Double(startX + 20, startY + 20, 60, 140);
-		Rectangle2D.Double pocket2 = new Rectangle2D.Double(startX + 520, startY + 20, 60, 140);
-
 		int dx = startX + 30;
 		int dy = startY + 30;
 		for (int i = 0; i < 12; i++) {
@@ -47,14 +34,19 @@ public class Mancala implements Icon{
 				dx -= 140;
 				dy += 75;
 			}
+			
 			Ellipse2D.Double pocket = new Ellipse2D.Double(dx, dy, 50, 50);
-			g2.draw(pocket);
+			pockets.add(pocket);
 			dy = startY + 30;
+		}
+		for (int i = 0; i < pockets.size(); i++){
+			g2.draw(pockets.get(i));
 		}
 		g2.draw(outline);
 		g2.draw(pocket1);
 		g2.draw(pocket2);
 		
 	}
-
 }
+	
+	
