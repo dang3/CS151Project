d71dce42e155e59e92a82797c6486b99bcc54f40
Dennis Dang <dang.dennis21@gmail.com>
2017-11-09 12:09:11 -0800

    moved mancala board to GameBoardPanel

--- src/GameBoardPanel.java
+++ src/GameBoardPanel.java
@@ -3,12 +3,16 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GameBoardPanel extends JPanel {
-	private MancalaBoard board = new MancalaBoard();
+	private Mancala gameBoard;
+	
+	public GameBoardPanel() {
+		gameBoard = new Mancala();
+		repaint();
+	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		board.draw(g);
-	
+		gameBoard.paintIcon(this, g, 1, 1);
 	}
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import javax.swing.JLabel;
 public class Main {
 
 	public static void main(String[] args) {
-		//Frame frame = new Frame();
-		JFrame f = new JFrame();
-		f.setPreferredSize(new Dimension(1000, 500));
-		Mancala m = new Mancala(20, 30);
-		JLabel label = new JLabel(m);
-		f.add(label);
+		Frame frame = new Frame();
+		//JFrame f = new JFrame();
+		//f.setPreferredSize(new Dimension(1000, 500));
+		//Mancala m = new Mancala();
+		//JLabel label = new JLabel(m);
+		//f.add(label);
 		
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.pack();
-		f.setVisible(true);
+//		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		f.pack();
+//		f.setVisible(true);
 		
 
 	}
