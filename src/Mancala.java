4bbf4a5275e30de2b02c2ecfaec9bc39890b34e5
Marissa Xiong <marxiong5@gmail.com>
2017-11-14 12:01:06 -0800

    uploaded files for pocket class

--- src/Mancala.java
+++ src/Mancala.java
@@ -1,27 +1,36 @@
 import java.awt.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
-import javax.swing.*;
 
-public class Mancala {
-	public ArrayList<Ellipse2D.Double> pockets;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Mancala{
+	ArrayList<Ellipse2D.Double> pockets;
+	ArrayList<Pocket> pocket_int;
 	JPanel label = new JPanel();
 	int startX = 150;
 	int startY = 100;
 	Rectangle2D.Double outline = new Rectangle2D.Double(startX, startY, 600, 180);
 	Rectangle2D.Double pocket1 = new Rectangle2D.Double(startX + 20, startY + 20, 60, 140);
 	Rectangle2D.Double pocket2 = new Rectangle2D.Double(startX + 520, startY + 20, 60, 140);
-	
+	//create pocket class
+	//change mancala class
 	
 	public Mancala(){
 		pockets = new ArrayList<>();
+		pocket_int = new ArrayList<>();
 	}
 
 	public void draw(Graphics g){
+		//int index = 0;
 		Graphics2D g2 = (Graphics2D) g;
 		int dx = startX + 30;
 		int dy = startY + 30;
 		for (int i = 0; i < 12; i++) {
+			Pocket p = new Pocket(i);
+			pocket_int.add(p);
 			dx += 70;
 			if (i == 6) {
 				dx -= 70;
@@ -42,11 +51,10 @@ public class Mancala {
 		g2.draw(outline);
 		g2.draw(pocket1);
 		g2.draw(pocket2);
+		
+		System.out.println(pocket_int.size());
+		
 	}
-	
-	
-	
-	
 }
 	
 	
---
+++ src/Pocket.java
@@ -0,0 +1,37 @@
+import java.awt.geom.Ellipse2D;
+
+public class Pocket {
+	int index;
+	//Ellipse2D.Double p;
+	
+	public Pocket(int index){
+		this.index = index;
+		//p = new Ellipse2D.Double(arg0, arg1, arg2, arg3)
+	}
+	
+	
+	
+	/** IGNORE THIS **/
+//	//methods to implement
+//	public void draw(){
+//		
+//	}
+//	
+//	public boolean contains(){
+//		
+//	}
+//	
+//	public boolean isSelected(){
+//		
+//	}
+//	
+//	// ??????
+//	public void setSelected(){
+//		
+//	}
+//	
+//	public void drawSelected(){
+//		
+//	}
+
+}
