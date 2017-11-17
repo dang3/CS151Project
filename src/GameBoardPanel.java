45a29513aebd82b19de81643b3a7990f4f7a81f9
verawong789 <verawong789@gmail.com>
2017-11-17 00:52:23 -0800

    added loop in Controller to update Model, added helper classes in Model

--- src/GameBoardPanel.java
+++ src/GameBoardPanel.java
@@ -10,12 +10,14 @@ import javax.swing.event.ChangeListener;
 public class GameBoardPanel extends JPanel implements ChangeListener {
 	private Mancala gameBoard;
 	private Model model;
+	boolean isPlayerA;
 	
 	public GameBoardPanel(Model model) {
 		this.model = model;
 		gameBoard = new Mancala();
 		repaint();
 		addMouseListener(new Listener());
+		isPlayerA=true;
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -42,6 +44,93 @@ public class GameBoardPanel extends JPanel implements ChangeListener {
 				// model.updateModel(gameBoard.pockets.indexOf(pocket));
 				System.out.println(gameBoard.pockets.indexOf(pocket));
 			}
+			
+			
+			//check if correct player's pits
+			int index = gameBoard.pockets.indexOf(pocket);
+			if (isPlayerA) {
+				if (index>5) {
+					System.out.println("You are Player A, please choose pits on your side. ");
+					//exit the method? want to skip actual update part
+				}
+			}
+			else{
+				if (index<7 || index>12){
+					System.out.println("You are Player B, please choose pits on your side. ");
+					//exit the method
+				}
+			}
+			
+			
+			//for loop until #of stones runs out
+			int nextPitIndex = index + 1;
+			int stoneNumber = model.getStoneNumber(index);
+			for (; stoneNumber>0; stoneNumber-- ) {//get method for stoneNumber in that pit?
+				
+				if (nextPitIndex==14) { //back to beginning of loop
+					nextPitIndex=0;
+				}
+				else if(nextPitIndex==6){ 
+					if (isPlayerA) {
+						model.updateModel(nextPitIndex);
+						if (stoneNumber==1) { //last stone
+							//free turn
+							//keep status the same for isPlayerA and in undo methods later
+							//update and 
+						}
+					}
+					else { //skip because player B
+						stoneNumber=stoneNumber+1; //add stone back to counter?
+					}
+				}
+				else if (nextPitIndex==13) {
+					if (!isPlayerA) {
+						model.updateModel(nextPitIndex);
+						if (stoneNumber==1) { //last stone
+							//free turn case again
+						}
+					}
+					else { //skip
+						stoneNumber=stoneNumber+1;
+					}
+				}
+				else {
+					if (stoneNumber==1) { //last stone
+						//dont updateModel yet
+						int across = 12-nextPitIndex;
+						int value;
+						if (isPlayerA && model.isSideA(nextPitIndex)) {
+							value = model.getStoneNumber(across); //get other side 
+							model.toZero(across); //toZero other side
+							value = value + 1 + model.getPlayerAMancala();
+							model.mancalaNewValue(true, value);
+							}
+						else if ((!isPlayerA) && (!model.isSideA(nextPitIndex))) {
+							value = model.getStoneNumber(across); //get other side 
+							model.toZero(across); //toZero other side
+							value = value + 1 + model.getPlayerBMancala();
+							model.mancalaNewValue(false, value);
+						}
+					}
+					else {
+						model.updateModel(nextPitIndex);
+					}
+				}
+				nextPitIndex++;
+			}
+			
+			//check if all pits on a side are empty 
+			if (model.sideAEmpty())  {
+				model.sideBIntoB();
+				//end?
+			}
+			else if (model.sideBEmpty()) {
+				model.sideAIntoA();
+				//end
+			}		
+			
+			//change player at end
+			isPlayerA = !isPlayerA;
 		}
 	}
 
@@ -51,5 +140,8 @@ public class GameBoardPanel extends JPanel implements ChangeListener {
 		// Mancala gameBoard reads from the Model how many seeds per pocket,
 		// then the View redraws
 		
+		for (int i = 0; i<14; i++) {
+			model.getStoneNumber(i); //connect to Mancala? for drawing how many stones
+		}
 	}
 }
--- src/Model.java
+++ src/Model.java
@@ -70,10 +70,88 @@ public class Model {
 	 */
 	public void updateModel(int index) {
 		// update the arrays here
+		if (index>=0 || index<=5) {
+			playerAPits[index]++;
+		}
+		else if (index==6) {
+			playerAMancala++;
+		}
+		else if (index>=7 || index<=12) {
+			playerBPits[index-7]++;
+		}
+		else {//index==13
+			playerBMancala++;
+		}
 		
 		// let the views know about the change so that they update
 		for(ChangeListener listener : listeners) {
 			listener.stateChanged( new ChangeEvent(this) );
+		}
+	}
+	
+	//for easy access to indices
+	public int getStoneNumber(int index) {
+		if (index>=0 || index<=5) {
+			return playerAPits[index];
+		}
+		else if (index==6) {
+			return playerAMancala;
+		}
+		else if (index>=7 || index<=12) {
+			return playerBPits[index-7];
+		}
+		else {//index==13
+			return playerBMancala;
+		}
+	}
+	
+	public boolean sideAEmpty() {
+		return (getStoneNumber(0)==0)&&(getStoneNumber(1)==0)&&(getStoneNumber(2)==0)&&(getStoneNumber(3)==0)&&(getStoneNumber(4)==0)&&(getStoneNumber(5)==0);
+	}
+	
+	public boolean sideBEmpty() {
+		return (getStoneNumber(7)==0)&&(getStoneNumber(8)==0)&&(getStoneNumber(9)==0)&&(getStoneNumber(10)==0)&&(getStoneNumber(11)==0)&&(getStoneNumber(12)==0);
+	}
+	
+	public void sideAIntoA() {
+		for (int i = 0; i<=5; i++) {
+			playerAMancala = playerAMancala + getStoneNumber(i);
+			playerAPits[i] = 0;
+		}
+	}
+	
+	public void sideBIntoB() {
+		for (int i = 7; i<=12; i++) {
+			playerBMancala = playerBMancala + getStoneNumber(i);
+			playerBPits[i] =0;
+		}
+	}
+	
+	public boolean isSideA(int index) {
+		if (index<6) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void toZero(int index) {
+		if (index<6) {
+			playerAPits[index] = 0;
+		}
+		else {
+			playerBPits[index] = 0;
+		}
+	}
+	
+	public void mancalaNewValue(boolean isA, int value) {
+		if (isA) {
+			playerAMancala = value;
+			
+		}
+		else {
+			playerBMancala = value;
 		}
 	}
 }
