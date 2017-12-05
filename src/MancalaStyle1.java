245d077051a9fafee56480ba9e6bb583b118a80c
Marissa Xiong <marxiong5@gmail.com>
2017-12-04 19:40:09 -0800

    styles

---
+++ src/MancalaStyle1.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class MancalaStyle1 implements Style {
+	public Color colorOfBoard(){
+		return Color.black;
+	}
+	public Color colorOfPockets() {
+		return Color.BLUE;
+	}
+}
